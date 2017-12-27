286153baa7c81da46facd90a992b8f2ef6475bfe
Nikita <nonikname1994@mail.ru>
2017-12-27 21:47:40 +0300

    Add files via upload

--- Binary_Search_Tree/Source.cpp
+++ Binary_Search_Tree/Source.cpp
@@ -7,7 +7,7 @@
 template< template<typename, typename> class DerivedNodeType, typename KeyType, typename ValueType>
 class BaseNode {
 private:
-	
+
 	using NodeType = DerivedNodeType<KeyType, ValueType>;
 	using NodePtrType = NodeType*;
 
@@ -28,6 +28,7 @@ protected:
 		;
 	}
 
+
 	explicit BaseNode(const NodeType &node, const NodePtrType parent = nullptr) :
 		parent(parent),
 		right(nullptr),
@@ -47,73 +48,164 @@ protected:
 	}
 
 public:
-	
+
 	KeyType key;
 	ValueType value;
 
 };
 
 
-template<typename NodeType>
-class InorderIterator {
+template< template<typename> class DerivedIteratorType, typename NodeType>
+class Iterator {
 private:
 
-	using IteratorType = InorderIterator<NodeType>;
+	using IteratorType = DerivedIteratorType<NodeType>;
 	using NodePtrType = NodeType*;
-	using NodeRefType = NodeType&;
+
+protected:
 
 	NodePtrType ptr;
 
+	explicit Iterator(const NodePtrType ptr_ = nullptr) :
+		ptr(ptr_) {
+		;
+	}
+
+	Iterator(const Iterator &iter) :
+		ptr(iter.ptr) {
+		;
+	}
+
 public:
 
-	explicit InorderIterator(const NodePtrType ptr = nullptr) : ptr(ptr) { ; }
-
-	InorderIterator(const IteratorType &iter) : ptr(iter.ptr), { ; }
-
-	const IteratorType &operator=(const IteratorType &iter) {
+	IteratorType &operator=(const IteratorType &iter) {
 		ptr = iter.ptr;
 		return *this;
 	}
 
-	const IteratorType &operator=(const NodePtrType node_ptr) {
-		ptr = node_ptr;
+	IteratorType &operator=(const NodePtrType ptr_) {
+		ptr = ptr_;
 		return *this;
 	}
 
+	operator bool() const {
+		return static_cast<bool>(ptr);
+	}
+
+	bool operator!() const {
+		return !(static_cast<bool>(ptr));
+	}
+
+	bool operator==(const IteratorType &iter) const {
+		return ptr == iter.ptr;
+	}
+
+	bool operator==(const NodePtrType ptr_) const {
+		return ptr == ptr_;
+	}
+
+	friend bool operator==(const NodePtrType ptr_, const IteratorType &iter);
+
+	bool operator!=(const IteratorType &iter) const {
+		return ptr != iter.ptr;
+	}
+
+	bool operator!=(const NodePtrType ptr_) const {
+		return ptr != ptr_;
+	}
+
+	friend bool operator!=(const NodePtrType ptr_, const IteratorType &iter);
+
+	bool operator&&(const IteratorType &iter) const {
+		return ptr && iter.ptr;
+	}
+
+	bool operator&&(const NodePtrType ptr_) const {
+		return ptr && ptr_;
+	}
+
+	friend bool operator&&(const NodePtrType ptr_, const IteratorType &iter);
+
+	bool operator||(const IteratorType &iter) const {
+		return ptr || iter.ptr;
+	}
+
+	bool operator||(const NodePtrType ptr_) const {
+		return ptr || ptr_;
+	}
+
+	friend bool operator||(const NodePtrType ptr_, const IteratorType &iter);
+
+	const NodePtrType get() const {
+		return ptr;
+	}
+};
+
+
+template< template<typename> class DerivedIteratorType, typename NodeType>
+inline bool operator==(const typename Iterator<DerivedIteratorType, NodeType>::NodePtrType ptr_, const typename Iterator<DerivedIteratorType, NodeType>::IteratorType &iter)
+{
+	return ptr_ == iter.ptr;
+}
+
+
+template< template<typename> class DerivedIteratorType, typename NodeType>
+inline bool operator!=(const typename Iterator<DerivedIteratorType, NodeType>::NodePtrType ptr_, const typename Iterator<DerivedIteratorType, NodeType>::IteratorType &iter)
+{
+	return ptr_ != iter.ptr;
+}
+
+
+template< template<typename> class DerivedIteratorType, typename NodeType>
+inline bool operator&&(const typename Iterator<DerivedIteratorType, NodeType>::NodePtrType ptr_, const typename Iterator<DerivedIteratorType, NodeType>::IteratorType &iter)
+{
+	return ptr_ && iter.ptr;
+}
+
+
+template< template<typename> class DerivedIteratorType, typename NodeType>
+inline bool operator||(const typename Iterator<DerivedIteratorType, NodeType>::NodePtrType ptr_, const typename Iterator<DerivedIteratorType, NodeType>::IteratorType &iter)
+{
+	return ptr_ || iter.ptr;
+}
+
+
+template<typename NodeType>
+class InorderIterator : public Iterator<InorderIterator, NodeType> {
+private:
+
+	using BaseIteratorType = Iterator<::InorderIterator, NodeType>;
+	using IteratorType = InorderIterator<NodeType>;
+	using NodePtrType = NodeType*;
+
+public:
+
+	explicit InorderIterator(const NodePtrType ptr_ = nullptr) :
+		BaseIteratorType(ptr_) {
+		;
+	}
+
+	InorderIterator(const InorderIterator &iter) :
+		BaseIteratorType(iter) {
+		;
+	}
+
 	IteratorType &operator++();
-
 	IteratorType operator++(int);
-
 	IteratorType &operator--();
-
 	IteratorType operator--(int);
-
-	const bool operator bool() const { return ptr; }
-
-	const bool operator!() const { return !ptr; }
-
-	const bool operator==(const IteratorType &iter) const { return ptr == iter.ptr; }
-
-	const bool operator==(const NodePtrType ptr_) const { return ptr == ptr_; }
-
-	friend const bool operator==(const NodePtrType ptr, const IteratorType &iter);
-
-	const NodePtrType pget() const { return ptr; }
-	
-	const NodeRefType get() const { return *ptr; }
 
 };
 
 template<typename NodeType>
 typename InorderIterator<NodeType>::IteratorType &InorderIterator<NodeType>::operator++()
 {
-
 	if (ptr) {
 
 		if (ptr->right) {
-			
+
 			ptr = ptr->right;
-			
+
 			while (ptr->left)
 				ptr = ptr->left;
 		}
@@ -210,57 +302,359 @@ typename InorderIterator<NodeType>::IteratorType InorderIterator<NodeType>::oper
 
 
 template<typename NodeType>
-inline const bool operator==(const typename InorderIterator<NodeType>::NodePtrType ptr, const typename InorderIterator<NodeType>::IteratorType &iter)
+class PreorderIterator : public Iterator<PreorderIterator, NodeType> {
+private:
+
+	using BaseIteratorType = Iterator<::PreorderIterator, NodeType>;
+	using IteratorType = PreorderIterator<NodeType>;
+	using NodePtrType = NodeType*;
+
+public:
+
+	explicit PreorderIterator(const NodePtrType ptr_ = nullptr) :
+		BaseIteratorType(ptr_) {
+		;
+	}
+
+	PreorderIterator(const PreorderIterator &iter) :
+		BaseIteratorType(iter) {
+		;
+	}
+
+	IteratorType &operator++();
+	IteratorType operator++(int);
+	IteratorType &operator--();
+	IteratorType operator--(int);
+
+};
+
+
+template<typename NodeType>
+typename PreorderIterator<NodeType>::IteratorType &PreorderIterator<NodeType>::operator++()
 {
-	return ptr == iter.ptr;;
+	if (ptr) {
+
+		if (ptr->left)
+			ptr = ptr->left;
+
+		else if (ptr->right)
+			ptr = ptr->right;
+
+		else {
+
+			while (true) {
+
+				if (ptr->parent == nullptr) {
+					ptr = nullptr;
+					break;
+				}
+
+				else if ( (ptr->parent->right == ptr) || (ptr->parent->right == nullptr) )
+					ptr = ptr->parent;
+
+				else {
+					ptr = ptr->parent->right;
+					break;
+				}
+			}
+		}
+	}
+
+	return *this;
+}
+
+template<typename NodeType>
+typename PreorderIterator<NodeType>::IteratorType PreorderIterator<NodeType>::operator++(int)
+{
+	NodePtrType previous_ptr = ptr;
+
+	if (ptr) {
+
+		if (ptr->left)
+			ptr = ptr->left;
+
+		else if (ptr->right)
+			ptr = ptr->right;
+
+		else {
+
+			while (true) {
+
+				if (ptr->parent == nullptr) {
+					ptr = nullptr;
+					break;
+				}
+
+				else if ((ptr->parent->right == ptr) || (ptr->parent->right == nullptr))
+					ptr = ptr->parent;
+
+				else {
+					ptr = ptr->parent->right;
+					break;
+				}
+			}
+		}
+	}
+
+	return IteratorType(previous_ptr);
+}
+
+
+template<typename NodeType>
+typename PreorderIterator<NodeType>::IteratorType &PreorderIterator<NodeType>::operator--()
+{
+	if (ptr) {
+
+		if (ptr->parent && (ptr->parent->right == ptr) && ptr->parent->left) {
+
+			ptr = ptr->parent->left;
+
+			while (true) {
+
+				if (ptr->right)
+					ptr = ptr->right;
+
+				else if (ptr->left)
+					ptr = ptr->left;
+
+				else break;
+			}
+		}
+		else ptr = ptr->parent;
+	}
+
+	return *this;
+}
+
+
+template<typename NodeType>
+typename PreorderIterator<NodeType>::IteratorType PreorderIterator<NodeType>::operator--(int)
+{
+	NodePtrType previous_ptr = ptr;
+
+	if (ptr) {
+
+		if (ptr->parent && (ptr->parent->right == ptr) && ptr->parent->left) {
+
+			ptr = ptr->parent->left;
+
+			while (true) {
+
+				if (ptr->right)
+					ptr = ptr->right;
+
+				else if (ptr->left)
+					ptr = ptr->left;
+
+				else break;
+			}
+		}
+		else ptr = ptr->parent;
+	}
+
+	return IteratorType(previous_ptr);
+}
+
+
+template<typename NodeType>
+class PostorderIterator : public Iterator<PostorderIterator, NodeType> {
+private:
+
+	using BaseIteratorType = Iterator<::PostorderIterator, NodeType>;
+	using IteratorType = PostorderIterator<NodeType>;
+	using NodePtrType = NodeType*;
+
+public:
+
+	explicit PostorderIterator(const NodePtrType ptr_ = nullptr) :
+		BaseIteratorType(ptr_) {
+		;
+	}
+
+	PostorderIterator(const PostorderIterator &iter) :
+		BaseIteratorType(iter) {
+		;
+	}
+
+	IteratorType &operator++();
+	IteratorType operator++(int);
+	IteratorType &operator--();
+	IteratorType operator--(int);
+
+};
+
+
+template<typename NodeType>
+typename PostorderIterator<NodeType>::IteratorType &PostorderIterator<NodeType>::operator++()
+{
+	if (ptr) {
+
+		if ((ptr->parent == nullptr) || (ptr->parent->right == ptr) || (ptr->parent->right == nullptr))
+			ptr = ptr->parent;
+		
+		else {
+
+			ptr = ptr->parent->right;
+
+			while (true) {
+
+				if (ptr->left)
+					ptr = ptr->left;
+
+				else if (ptr->right)
+					ptr = ptr->right;
+
+				else break;
+			}
+		}
+	}
+
+	return *this;
+}
+
+template<typename NodeType>
+typename PostorderIterator<NodeType>::IteratorType PostorderIterator<NodeType>::operator++(int)
+{
+	NodePtrType previous_ptr = ptr;
+
+	if (ptr) {
+
+		if ((ptr->parent == nullptr) || (ptr->parent->right == ptr) || (ptr->parent->right == nullptr))
+			ptr = ptr->parent;
+
+		else {
+
+			ptr = ptr->parent->right;
+
+			while (true) {
+
+				if (ptr->left)
+					ptr = ptr->left;
+
+				else if (ptr->right)
+					ptr = ptr->right;
+
+				else break;
+			}
+		}
+	}
+
+	return IteratorType(previous_ptr);
+}
+
+
+template<typename NodeType>
+typename PostorderIterator<NodeType>::IteratorType &PostorderIterator<NodeType>::operator--()
+{
+	if (ptr) {
+
+		if (ptr->right) 
+			ptr = ptr->right;
+
+		else if (ptr->left) 
+			ptr = ptr->left;
+
+		else {
+
+			while (true) {
+
+				if (ptr->parent == nullptr) {
+					ptr = nullptr;
+					break;
+				}
+				else if ( (ptr->parent->left == ptr) || (ptr->parent->left == nullptr) )
+					ptr = ptr->parent;
+
+				else {
+					ptr = ptr->parent->left;
+					break;
+				}
+			}
+		}
+	}
+
+	return *this;
+}
+
+
+template<typename NodeType>
+typename PostorderIterator<NodeType>::IteratorType PostorderIterator<NodeType>::operator--(int)
+{
+	NodePtrType previous_ptr = ptr;
+
+	if (ptr) {
+
+		if (ptr->right)
+			ptr = ptr->right;
+
+		else if (ptr->left)
+			ptr = ptr->left;
+
+		else {
+
+			while (true) {
+
+				if (ptr->parent == nullptr) {
+					ptr = nullptr;
+					break;
+				}
+				else if ((ptr->parent->left == ptr) || (ptr->parent->left == nullptr))
+					ptr = ptr->parent;
+
+				else {
+					ptr = ptr->parent->left;
+					break;
+				}
+			}
+		}
+	}
+
+	return IteratorType(previous_ptr);
 }
 
 
 
 
 
-
-
-
-
-
-
-template< template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
+// update - add min key value, max key value, head value and others
+template< template<typename, typename> class DerivedTreeType, template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
 class BinarySearchTree {
 private:
 
 	using NodeType = TreeNodeType<KeyType, ValueType>;
 	using NodePtrType = NodeType*;
-	using NodeRefType = NodeType&;
-	using TreeType = BinarySearchTree<TreeNodeType, KeyType, ValueType>;
+	using TreeType = DerivedTreeType<KeyType, ValueType>;
 
 protected:
 
 	NodePtrType head;
 
-	BinarySearchTree() : head(nullptr) { ; }
+	BinarySearchTree() :
+		head(nullptr) {
+		;
+	}
 
-	explicit BinarySearchTree(const KeyType &key, const ValueType &value) { head = new NodeType(key, value); }
+	explicit BinarySearchTree(const KeyType &key, const ValueType &value) {
+		head = new NodeType(key, value);
+	}
 
-	explicit BinarySearchTree(const TreeType &tree);
+	BinarySearchTree(const BinarySearchTree &tree);
 
 	~BinarySearchTree();
 
 public:
 
-	const NodePtrType pfind(const KeyType &key) const;
+	TreeType &operator=(const TreeType &tree);
 
-	const NodePtrType pfind(const KeyType *key) const;
+	const NodePtrType find(const KeyType &key) const;
 
-	const NodeRefType find(const KeyType &key) const;
-
-	const NodeRefType find(const KeyType *key) const;
+	const NodePtrType find(const KeyType *key) const;
 
 };
 
 
-template< template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
-BinarySearchTree<TreeNodeType, KeyType, ValueType>::BinarySearchTree(const TreeType &tree)
+template< template<typename, typename> class DerivedTreeType, template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
+BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::BinarySearchTree(const BinarySearchTree &tree)
 {
 	if (!tree.head) {
 		head = nullptr;
@@ -311,74 +705,141 @@ BinarySearchTree<TreeNodeType, KeyType, ValueType>::BinarySearchTree(const TreeT
 }
 
 
-template< template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
-BinarySearchTree<TreeNodeType, KeyType, ValueType>::~BinarySearchTree()
+template< template<typename, typename> class DerivedTreeType, template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
+BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::~BinarySearchTree()
 {
-	while (head->parent || head->left || head->right) {
+	if (head) {
 
-		if (head->left) head = head->left;
+		while (true) {
 
-		else if (head->right) head = head->right;
+			if (head->left) 
+				head = head->left;
 
-		else {
+			else if (head->right) 
+				head = head->right;
 
-			head = head->parent;
+			else if (head->parent) {
 
-			if (head->left) {
-				delete head->left;
-				head->left = nullptr;
+				head = head->parent;
+
+				if (head->left) {
+					delete head->left;
+					head->left = nullptr;
+				}
+				else {
+					delete head->right;
+					head->right = nullptr;
+				}
 			}
-			else {
-				delete head->right;
-				head->right = nullptr;
+			else break;
+		}
+
+		delete head;
+	}
+}
+
+
+template< template<typename, typename> class DerivedTreeType, template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
+typename BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::TreeType &BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::operator=(const BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::TreeType &tree)
+{
+	if (this != &tree) {
+
+		// delete current tree nodes (see destructor)
+		if (head) {
+
+			while (true) {
+
+				if (head->left)
+					head = head->left;
+
+				else if (head->right)
+					head = head->right;
+
+				else if (head->parent) {
+
+					head = head->parent;
+
+					if (head->left) {
+						delete head->left;
+						head->left = nullptr;
+					}
+					else {
+						delete head->right;
+						head->right = nullptr;
+					}
+				}
+				else break;
+			}
+
+			delete head;
+			head = nullptr;
+		}
+
+		// copy new nodes if needed
+		if (tree.head) {
+			
+			enum {
+				RIGHT_SUB_TREE,
+				LEFT_SUB_TREE,
+				PARENT
+			} from = PARENT;
+
+			const NodePtrType ptr = tree.head;
+			head = new NodeType(ptr);
+
+			while (ptr->parent || from) {
+
+				switch (from) {
+
+				case RIGHT_SUB_TREE:
+
+					from = (ptr == ptr->parent->left) ? LEFT_SUB_TREE : RIGHT_SUB_TREE;
+					ptr = ptr->parent;
+					head = head->parent;
+					break;
+
+				case LEFT_SUB_TREE:
+
+					if (ptr->right) {
+						from = PARENT;
+						ptr = ptr->right;
+						head = head->right = new NodeType(ptr, head);
+					}
+					else from = RIGHT_SUB_TREE;
+					break;
+
+				case PARENT:
+
+					if (ptr->left) {
+						from = PARENT;
+						ptr = ptr->left;
+						head = head->left = new NodeType(ptr, head);
+					}
+					else from = LEFT_SUB_TREE;
+					break;
+				}
 			}
 		}
 	}
 
-	delete head;
+	return *this;
 }
 
 
-template< template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
-const typename BinarySearchTree<TreeNodeType, KeyType, ValueType>::NodeRefType BinarySearchTree<TreeNodeType, KeyType, ValueType>::find(const KeyType &key) const
-{
-	const NodePtrType ptr = head;
-	
-	while (ptr && (key != ptr->key) ) 
-		ptr = (key < ptr->key) ? ptr->left : ptr->right;
-	
-
-	return *ptr;
-}
-
-
-template< template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
-const typename BinarySearchTree<TreeNodeType, KeyType, ValueType>::NodeRefType BinarySearchTree<TreeNodeType, KeyType, ValueType>::find(const KeyType *key) const
-{
-	const NodePtrType ptr = head;
-
-	while (ptr && (*key != ptr->key)) 
-		ptr = (*key < ptr->key) ? ptr->left : ptr->right;
-
-	return *ptr;
-}
-
-
-template< template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
-const typename BinarySearchTree<TreeNodeType, KeyType, ValueType>::NodePtrType BinarySearchTree<TreeNodeType, KeyType, ValueType>::pfind(const KeyType &key) const
+template< template<typename, typename> class DerivedTreeType, template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
+const typename BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::NodePtrType BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::find(const KeyType &key) const
 {
 	const NodePtrType ptr = head;
 
 	while (ptr && (key != ptr->key))
 		ptr = (key < ptr->key) ? ptr->left : ptr->right;
 
-
 	return ptr;
 }
 
 
-template< template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
-const typename BinarySearchTree<TreeNodeType, KeyType, ValueType>::NodePtrType BinarySearchTree<TreeNodeType, KeyType, ValueType>::pfind(const KeyType *key) const
+template< template<typename, typename> class DerivedTreeType, template<typename, typename> class TreeNodeType, typename KeyType, typename ValueType>
+const typename BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::NodePtrType BinarySearchTree<DerivedTreeType, TreeNodeType, KeyType, ValueType>::find(const KeyType *key) const
 {
 	const NodePtrType ptr = head;
 
@@ -389,26 +850,37 @@ const typename BinarySearchTree<TreeNodeType, KeyType, ValueType>::NodePtrType B
 }
 
 
+
+// update simplenode types
 template<typename KeyType, typename ValueType>
 class SimpleNode : public BaseNode<SimpleNode, KeyType, ValueType> {
 private:
-
-	friend class BinarySearchTree<::SimpleNode, KeyType, ValueType>;
 
 	using BaseNodeType = BaseNode<::SimpleNode, KeyType, ValueType>;
 	using NodeType = SimpleNode<KeyType, ValueType>;
 	using NodePtrType = NodeType*;
 
+	friend class BinarySearchTree<::SimpleNode, KeyType, ValueType>;
+	friend class InorderIterator<NodeType>;
+
 	SimpleNode() = default;
 
 protected:
 
-	explicit SimpleNode(const KeyType &key, const ValueType &value) : BaseNodeType(key, value) { ; }
+	explicit SimpleNode(const KeyType &key, const ValueType &value) :
+		BaseNodeType(key, value) {
+		;
+	}
 
-	explicit SimpleNode(const NodeType &node, const NodePtrType parent = nullptr) : BaseNodeType(node, parent) { ; }
+	SimpleNode(const NodeType &node, const NodePtrType parent = nullptr) :
+		BaseNodeType(node, parent) {
+		;
+	}
 
-	explicit SimpleNode(const NodePtrType node, const NodePtrType parent = nullptr) : BaseNodeType(node, parent) { ; }
-
+	explicit SimpleNode(const NodePtrType node, const NodePtrType parent = nullptr) :
+		BaseNodeType(node, parent) {
+		;
+	}
 };
 
 
@@ -445,9 +917,12 @@ Point2D foo()
 
 int main() {
 
+	int *a = nullptr;
+	
 	std::cout << foo().x << std::endl;
 
 	Sleep(2000);
 	return 0;
 }
+
 
